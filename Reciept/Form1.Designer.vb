71627babdb6b149701c98ad69699e367aeeb6b26
kAlipay <richardabelanio@gmail.com>
2024-10-02 16:59:49 +0800

    sample only to print

---
+++ Reciept/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnprint = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(567, 259)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "ITEM"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "QTY"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "PRICE"
+        Me.Column3.Name = "Column3"
+        '
+        'btnprint
+        '
+        Me.btnprint.Location = New System.Drawing.Point(12, 288)
+        Me.btnprint.Name = "btnprint"
+        Me.btnprint.Size = New System.Drawing.Size(100, 39)
+        Me.btnprint.TabIndex = 1
+        Me.btnprint.Text = "PRINT"
+        Me.btnprint.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(690, 364)
+        Me.Controls.Add(Me.btnprint)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents btnprint As Button
+End Class
---
+++ Reciept/Form1.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Drawing.Printing
+Public Class Form1
+    Dim WithEvents PD As New PrintDocument
+    Dim PPD As New PrintPreviewDialog
+    Dim longpaper As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Sub changelongpaper()
+        Dim rowcount As Integer
+        longpaper = 0
+        rowcount = DataGridView1.Rows.Count
+        longpaper = rowcount * 15
+        longpaper = longpaper + 500
+
+    End Sub
+
+    Private Sub btnprint_Click(sender As Object, e As EventArgs) Handles btnprint.Click
+        changelongpaper()
+        PPD.Document = PD
+        PPD.ShowDialog()
+
+    End Sub
+
+    Private Sub PD_BeginPrint(sender As Object, e As PrintEventArgs) Handles PD.BeginPrint
+        Dim pagesetup As New PageSettings
+        pagesetup.PaperSize = New PaperSize("Custom", 250, longpaper)
+        PD.DefaultPageSettings = pagesetup
+
+    End Sub
+
+    Private Sub PD_PrintPage(sender As Object, e As PrintPageEventArgs) Handles PD.PrintPage
+        Dim f8 As New Font("Calibri", 8, FontStyle.Regular)
+        Dim f10 As New Font("Calibri", 10, FontStyle.Regular)
+        Dim f10b As New Font("Calibri", 10, FontStyle.Bold)
+        Dim f14 As New Font("Calibri", 14, FontStyle.Bold)
+
+        Dim leftmargin As Integer = PD.DefaultPageSettings.Margins.Left
+        Dim centermargin As Integer = PD.DefaultPageSettings.PaperSize.Width / 2
+        Dim rightmargin As Integer = PD.DefaultPageSettings.PaperSize.Width
+
+        Dim right As New StringFormat
+        Dim center As New StringFormat
+
+        right.Alignment = StringAlignment.Far
+        center.Alignment = StringAlignment.Center
+
+        Dim line As String
+        line = " ________________________________________________________ "
+
+        e.Graphics.DrawString("Store", f14, Brushes.Black, centermargin, 5, center)
+        e.Graphics.DrawString("REDGE ESPIRITU STORE", f10, Brushes.Black, centermargin, 25, center)
+        e.Graphics.DrawString("Tel + 0900000000", f10, Brushes.Black, centermargin, 40, center)
+
+        e.Graphics.DrawString("Invoice #", f8, Brushes.Black, 0, 60)
+        e.Graphics.DrawString(":", f8, Brushes.Black, 50, 60)
+        e.Graphics.DrawString("Tel + 0900000000", f8, Brushes.Black, 70, 60)
+
+        e.Graphics.DrawString("Casher #", f8, Brushes.Black, 0, 75)
+        e.Graphics.DrawString(":", f8, Brushes.Black, 50, 75)
+        e.Graphics.DrawString("Clyde Richard L. Abelanio", f8, Brushes.Black, 70, 75)
+
+        e.Graphics.DrawString("Date" & Date.Now(), f8, Brushes.Black, 0, 90)
+        e.Graphics.DrawString(line, f8, Brushes.Black, 0, 100)
+
+
+        e.Graphics.DrawString("QTY", f10b, Brushes.Black, 0, 110)
+        e.Graphics.DrawString("Item", f10b, Brushes.Black, 30, 110)
+        e.Graphics.DrawString("Price", f10b, Brushes.Black, rightmargin, 110, right)
+        Dim height As Integer
+        Dim i As Long
+
+        DataGridView1.AllowUserToAddRows = False
+
+        For row As Integer = 0 To DataGridView1.RowCount - 1
+            height += 15
+            e.Graphics.DrawString(DataGridView1.Rows(row).Cells(1).Value.ToString, f10, Brushes.Black, 0, 120 + height)
+            e.Graphics.DrawString(DataGridView1.Rows(row).Cells(0).Value.ToString, f10, Brushes.Black, 25, 120 + height)
+
+            i = DataGridView1.Rows(row).Cells(2).Value
+            DataGridView1.Rows(row).Cells(2).Value = Format(i, "##,##0")
+            e.Graphics.DrawString(DataGridView1.Rows(row).Cells(2).Value.ToString, f10, Brushes.Black, rightmargin, 120 + height, right)
+        Next
+
+        Dim height2 As Integer
+
+        height2 = 130 + height
+
+        sumprice()
+        e.Graphics.DrawString("Total: " & Format(t_price, "##,##0"), f10b, Brushes.Black, rightmargin, 10 + height2, right)
+        e.Graphics.DrawString(t_qty, f10b, Brushes.Black, 0, 10 + height2)
+
+        e.Graphics.DrawString(line, f8, Brushes.Black, centermargin, 35 + height2, center)
+        e.Graphics.DrawString("Thanks for shopping", f10b, Brushes.Black, centermargin, 50 + height2, center)
+        e.Graphics.DrawString("Redge Espiritu Store", f10b, Brushes.Black, centermargin, 65 + height2, center)
+    End Sub
+    Dim t_price As Long
+    Dim t_qty As Long
+    Sub sumprice()
+        Dim countprice As Long = 0
+
+        For rowitem As Long = 0 To DataGridView1.RowCount - 1
+            countprice = countprice + Val(DataGridView1.Rows(rowitem).Cells(2).Value * DataGridView1.Rows(rowitem).Cells(1).Value)
+        Next
+        t_price = countprice
+
+        Dim countqty As Long = 0
+        For rowitem As Long = 0 To DataGridView1.RowCount - 1
+            countqty = countqty + DataGridView1.Rows(rowitem).Cells(1).Value
+        Next
+        t_qty = countqty
+
+    End Sub
+End Class
